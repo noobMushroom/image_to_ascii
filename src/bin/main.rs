d603825aab429c7676f151a51e293b40312442b5
noobMushroom <mushroom019.coder@gmail.com>
2024-05-21 20:07:51 +0530

    added readme file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 fn main() {
     // let img_path =
-    //     Path::new("");
+    //     Path::new(" ");
     let char_map = AsciiChars::new(' ', '.', '*', '#');
     println!("{}", create_ascii(img_path, char_map).unwrap());
     // _ = create_ascii(img_path, char_map).unwrap();
